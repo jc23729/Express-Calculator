66f29d57803ace50a25ced9ec2dac8d99855681e
Juan Cruz <jc23729@gmail.com>
2021-03-20 11:47:54 -0400

    import helper functions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const ExpressError = require('./expressError');
 
+const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require('./helpers');
+
 
 
 
