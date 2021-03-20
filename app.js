fc600400e267f163468495090415d93d29f21c35
Juan Cruz <jc23729@gmail.com>
2021-03-20 11:35:58 -0400

    update

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const app = express();
+const ExpressError = require('./expressError');
 
-app.get('/dogs', function(request, response) {
+app.get('/mean', function(request, response) {
+    return response.send('Dogs go brk brk');
+  });
+
+app.get('/median', function(request, response) {
   return response.send('Dogs go brk brk');
 });
+
+app.get('/mode', function(request, response) {
+    return response.send('Dogs go brk brk');
+  });
 
 app.listen(3000, function(){
   console.log('App on port 3000');
